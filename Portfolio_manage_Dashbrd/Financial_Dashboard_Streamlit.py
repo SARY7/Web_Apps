188a555f75c13c906321b4222f098333b0c7fa97
Saeed Rahimi <rahimi.saeed.gm@gmail.com>
2021-09-18 17:16:53 +0430

    Code

--- Portfolio_manage_Dashbrd/Financial_Dashboard_Streamlit.py
+++ Portfolio_manage_Dashbrd/Financial_Dashboard_Streamlit.py
@@ -1,13 +1,25 @@
 
 import streamlit as st
+import pandas as pd
 
 st.title("this is the title")
 st.header("this the header")
 st.subheader("this a subheader")
-st.write("this is a regular tex")
 
 """
 # header
 ## subheader
 """
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fimg.pink%2Fimage%2Ffv&psig=AOvVaw1adDbQdPzS1pUBZULR6kOp&ust=1616261994512000&source=images&cd=vfe&ved=0CAMQjB1qFwoTCJiMsKPzvO8CFQAAAAAdAAAAABAD")
+st.title('Portfolio Management Dashboard')
+
+ticker = ('BTC-USD', 'ETH-USD')
+
+dropdown = st.multiselect('Pick your assets', ticker)
+
+start = st.date_input('Start', value= pd.to_datetime('2021-09-09'))
+end = st.date_input('End', value= pd.to_datetime('today'))
+
+if len(dropdown) > 0:
+    df = yf.download(dropdown, start, end)['Adlj Close']
+    st.line_chart(df)
+
