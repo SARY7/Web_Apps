9ab709e06e1fb987e0181a6d9833ced39c4488aa
Saeed Rahimi <rahimi.saeed.gm@gmail.com>
2021-09-18 20:53:18 +0430

    SignIn/SignUp Page Skin

--- Portfolio_manage_Dashbrd/Financial_Dashboard_Streamlit.py
+++ Portfolio_manage_Dashbrd/Financial_Dashboard_Streamlit.py
@@ -1,25 +1,58 @@
-
 import streamlit as st
+import yfinance as yf
 import pandas as pd
 
-st.title("this is the title")
-st.header("this the header")
-st.subheader("this a subheader")
 
-"""
-# header
-## subheader
-"""
-st.title('Portfolio Management Dashboard')
+def Signinpage():
+    st.title("Welcome back, please Log In")
 
-ticker = ('BTC-USD', 'ETH-USD')
+    signin_menu = ["SignIn", "SignUp"]
+    signin_select = st.sidebar.selectbox("SignIn menu", signin_menu)
 
-dropdown = st.multiselect('Pick your assets', ticker)
+    if signin_select == "SignIn":
+        st.sidebar.subheader("SignIn to your account:")
 
-start = st.date_input('Start', value= pd.to_datetime('2021-09-09'))
-end = st.date_input('End', value= pd.to_datetime('today'))
+        username = st.sidebar.text_input("User Name")
+        password = st.sidebar.text_input("Password", type='password')
+        if st.sidebar.button("SignIn"):
+            if password == "12345":
+                st.success("Hi {}, its great to have you back :)".format(username))
+            else:
+                st.warning("Oops, the username/password is incorrect :( , try again.")
 
-if len(dropdown) > 0:
-    df = yf.download(dropdown, start, end)['Adlj Close']
-    st.line_chart(df)
+    elif signin_select == "SignUp":
+        st.subheader("Create your new account:")
 
+
+# if __name__ == '__Signinpage__':
+Signinpage()
+# st.title('Portfolio Management Dashboard')
+#
+# ticker = ('BTC-USD', 'ETH-USD')
+#
+# dropdown = st.multiselect('Pick your assets', ticker)
+#
+# start = st.date_input('Start', value=pd.to_datetime('2021-09-09'))
+# end = st.date_input('End', value=pd.to_datetime('today'))
+#
+# # if len(dropdown) > 0:
+# #     df = yf.download(dropdown, start, end)['Adlj Close']
+# #     st.line_chart(df)
+#
+# """
+# # header
+# """
+#
+# options = st.selectbox("which", ("hi", "car"))
+#
+# if options == "car":
+#     st.image("https://static.dw.com/image/51817152_303.jpg")
+#
+# st.sidebar.title("t")
+# choose = st.sidebar.write("test")
+#
+# sidechoose = st.sidebar.selectbox("which", ("hi", "car", "selecr"))
+#
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)
+#
