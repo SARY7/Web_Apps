16e616468f4a126ddd422e6dd057d826d04c9602
Saeed Rahimi <rahimi.saeed.gm@gmail.com>
2021-09-18 16:22:47 +0430

    Create Financial_Dashboard_Streamlit.py
    create Python script

---
+++ Portfolio_manage_Dashbrd/Financial_Dashboard_Streamlit.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+
+st.title("this is the title")
+st.header("this the header")
+st.subheader("this a subheader")
+st.write("this is a regular tex")
+
+"""
+# header
+## subheader
+"""
+st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fimg.pink%2Fimage%2Ffv&psig=AOvVaw1adDbQdPzS1pUBZULR6kOp&ust=1616261994512000&source=images&cd=vfe&ved=0CAMQjB1qFwoTCJiMsKPzvO8CFQAAAAAdAAAAABAD")
